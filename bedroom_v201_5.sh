3ca01cdaa765de9aef4adff2695d1d2f7c06f1e2
ankursikarwar <ankursikarwardc@gmail.com>
2025-09-12 21:25:47 -0400

    Update generation scripts

---
+++ bedroom_v201_5.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=Bedroom-v201-Part5
+#SBATCH --partition=long
+#SBATCH -c 48
+#SBATCH --output=/network/scratch/a/ankur.sikarwar/infinigen/slurm_logs_output/bedroom_v201_part5_job_output-%j.txt
+#SBATCH --error=/network/scratch/a/ankur.sikarwar/infinigen/slurm_logs_error/bedroom_v201_part5_job_error-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=512Gb
+
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
+
+module load anaconda/3
+conda activate infinigen
+
+
+python -m infinigen.datagen.manage_jobs \
+    --output_folder outputs/Bedroom_v201_Part5 \
+    --num_scenes 50000 \
+    --configs singleroom.gin studio.gin \
+    --pipeline_overrides \
+        get_cmd.driver_script=infinigen_examples.generate_indoors \
+        LocalScheduleHandler.use_gpu=True \
+        iterate_scene_tasks.n_camera_rigs=2 \
+    --overrides \
+        compose_indoors.terrain_enabled=False \
+        restrict_solving.restrict_parent_rooms=\[\"Bedroom\"\] \
+        compose_indoors.place_2=True \
+    --pipeline_configs \
+        local_256GB_custom.gin \
+        monocular.gin \
+        opengl_gt.gin \
+        indoor_background_configs.gin
---
+++ livingroom_v201_4.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --job-name=LivingRoom-v201-Part4
+#SBATCH --partition=long
+#SBATCH -c 48
+#SBATCH --output=/network/scratch/a/ankur.sikarwar/infinigen/slurm_logs_output/living_room_v201_part4_job_output-%j.txt
+#SBATCH --error=/network/scratch/a/ankur.sikarwar/infinigen/slurm_logs_error/living_room_v201_part4_job_error-%j.txt
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=512Gb
+
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
+
+module load anaconda/3
+conda activate infinigen
+
+
+python -m infinigen.datagen.manage_jobs \
+    --output_folder outputs/LivingRoom_v201_Part4 \
+    --num_scenes 50000 \
+    --configs singleroom.gin studio.gin \
+    --pipeline_overrides \
+        get_cmd.driver_script=infinigen_examples.generate_indoors \
+        LocalScheduleHandler.use_gpu=True \
+        iterate_scene_tasks.n_camera_rigs=2 \
+    --overrides \
+        compose_indoors.terrain_enabled=False \
+        restrict_solving.restrict_parent_rooms=\[\"LivingRoom\"\] \
+        compose_indoors.place_2=True \
+    --pipeline_configs \
+        local_256GB_custom.gin \
+        monocular.gin \
+        opengl_gt.gin \
+        indoor_background_configs.gin
